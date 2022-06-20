631c8e020af0ee502d9a09c662bdf5bbac0bfe3e
Minoru Nakano <minoru.nakano@gmail.com>
2022-06-20 18:00:57 -0400

    initial commit

---
+++ scripts/Format_CLIN.R
@@ -0,0 +1,34 @@
+args <- commandArgs(trailingOnly = TRUE)
+input_dir <- args[1]
+output_dir <- args[2]
+
+source("https://raw.githubusercontent.com/BHKLAB-Pachyderm/ICB_Common/main/code/Get_Response.R")
+
+clin = read.csv( file.path(input_dir, "CLIN.txt"), stringsAsFactors=FALSE , sep="\t" , dec=',')
+
+clin = cbind( clin[ , c( "pair_id","cancer_type","age_start_io","sex","RECIST","os_days","os_censor","pfs_days","pfs_censor","histology","drug_type" ) ], NA, NA, NA, NA, NA )
+colnames(clin) = c( "patient" , "primary" , "age" , "sex" , "recist" , "t.os"  ,"os","t.pfs", "pfs" , "histo" , "drug_type" , "stage" , "dna" , "rna" , "response.other.info" , "response")
+
+clin$drug_type = ifelse( clin$drug_type %in% "anti-CTLA-4" , "CTLA4" , 
+					ifelse( clin$drug_type %in% "anti-CTLA-4 + anti-PD-1/PD-L1", "Combo" , "PD-1/PD-L1" ))
+clin$sex = ifelse(clin$sex %in% "FEMALE" , "F" , "M")
+
+clin$t.os = clin$t.os/30.5
+clin$t.pfs = clin$t.pfs/30.5
+
+clin$recist[ clin$recist %in% "X" ] = NA 
+clin$response = Get_Response( data=clin )
+
+clin$patient = sapply( clin$patient , function(x){ paste( unlist( strsplit( as.character( x ) , "-" , fixed=TRUE )) , collapse=".") })
+
+case = read.csv( file.path(output_dir, "cased_sequenced.csv"), stringsAsFactors=FALSE , sep=";" )
+clin$dna[ clin$patient %in% case[ case$snv %in% 1 , ]$patient ] = "wes"
+
+clin = clin[ , c("patient" , "sex" , "age" , "primary" , "histo" , "stage" , "response.other.info" , "recist" , "response" , "drug_type" , "dna" , "rna" , "t.pfs" , "pfs" , "t.os" , "os" ) ]
+
+clin$primary[ clin$primary %in% "Anal" ] = "Colon"
+clin$primary[ clin$primary %in% "HNSCC" ] = "HNC"
+clin$primary[ clin$primary %in% "Sarcoma" ] = "Other"
+
+write.table( clin , file=file.path(output_dir, "CLIN.csv") , quote=FALSE , sep=";" , col.names=TRUE , row.names=FALSE )
+
